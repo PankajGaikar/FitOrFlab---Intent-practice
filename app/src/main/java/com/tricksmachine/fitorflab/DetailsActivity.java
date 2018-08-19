f794412c2f86dbdb85a3bba16476216533591bc1
PANKYAAA <pankajs.gaikar@gmail.com>
2018-08-19 15:22:52 +0530

    Dynamic context on second screen. Final commit.

--- app/src/main/java/com/tricksmachine/fitorflab/DetailsActivity.java
+++ app/src/main/java/com/tricksmachine/fitorflab/DetailsActivity.java
@@ -1,5 +1,7 @@
 package com.tricksmachine.fitorflab;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
@@ -14,9 +16,23 @@ public class DetailsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_details);
 
         TextView titleView = (TextView)findViewById(R.id.titleView);
-        ImageView imageView = (ImageView)findViewById(R.id.imageView);
+        ImageView imageView = (ImageView)findViewById(R.id.detailImageView);
         RelativeLayout backgroundView = (RelativeLayout)findViewById(R.id.backgroundViewLayout);
 
+        String titleString = getIntent().getStringExtra(MainActivity.EXTRA);
+        titleView.setText(titleString);
 
+        if( titleString.equalsIgnoreCase(MainActivity.EXCERSIZE_WEIGHT)){
+            imageView.setImageDrawable(getResources().getDrawable(R.drawable.weight, getApplicationContext().getTheme()));
+            backgroundView.setBackgroundColor(getResources().getColor(android.R.color.holo_blue_bright));
+        }
+        else if( titleString.equalsIgnoreCase(MainActivity.EXCERSIZE_CARDIO)){
+            imageView.setImageDrawable(getResources().getDrawable(R.drawable.heart, getApplicationContext().getTheme()));
+            backgroundView.setBackgroundColor(getResources().getColor(android.R.color.holo_orange_light));
+        }
+        else{
+            imageView.setImageDrawable(getResources().getDrawable(R.drawable.lotus, getApplicationContext().getTheme()));
+            backgroundView.setBackgroundColor(getResources().getColor(android.R.color.holo_red_light));
+        }
     }
 }
